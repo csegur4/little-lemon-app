1cae277ae6e74de99b4fe5d767ee991b401b9e0e
Carlos Segura Vidal <cibercarlossv@gmail.com>
2023-01-07 01:44:43 -0500

    nav fixed hmenu

--- src/components/booking/BookingHeader.js
+++ src/components/booking/BookingHeader.js
@@ -30,6 +30,8 @@ export default function BookingHeader(){
                         </div>
                 </div>
             </div>
+            <div>
+            </div>
         </header>
     )
 }
